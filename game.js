81512f5b424de1fb2604add83500107457e461ab
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-09-29 14:50:14 +1300

    Finalise algorithm

--- game.js
+++ game.js
@@ -2,9 +2,10 @@
 let board = [
     ['E', 'E', 'E'],
     ['E', 'E', 'E'],
-    ['E', 'E', 'E'],
+    ['E', 'E', 'M'],
 ];
-const click = { board: board, row: 1, col: 1 };
+console.table(board);
+const click = { board: board, row: 2, col: 2 };
 function updateEmptySquare({ board, row, col }) {
     if (board[row][col] === 'M') {
         board[row][col] = 'X';
@@ -15,9 +16,9 @@ function updateEmptySquare({ board, row, col }) {
         let adjacentMines = 0;
         for (let x = -1; x < 2; x++) {
             for (let y = -1; y < 2; y++) {
-                if ((row + x) < 0 || (row + x) > board.length)
+                if ((row + x) < 0 || (row + x) > board.length - 1)
                     continue;
-                if (col + y < 0 || col + y > board[0].length)
+                if (col + y < 0 || col + y > board[0].length - 1)
                     continue;
                 if (board[row + x][col + y] === 'M')
                     adjacentMines += 1;
@@ -30,10 +31,13 @@ function updateEmptySquare({ board, row, col }) {
                 for (let y = -1; y < 2; y++) {
                     if (x === 0 && y === 0)
                         continue;
-                    if ((row + x) < 0 || (row + x) > board.length)
+                    if ((row + x) < 0 || (row + x) > board.length - 1)
                         continue;
-                    if ((col + y) < 0 || (col + y) > board[0].length)
+                    if ((col + y) < 0 || (col + y) > board[0].length - 1)
                         continue;
+                    if (board[row + x][col + y] !== 'E')
+                        continue;
+                    updateEmptySquare({ board: board, row: row + x, col: col + y });
                 }
             }
         }
--- game.ts
+++ game.ts
@@ -8,10 +8,12 @@ let board =
 [
   ['E', 'E', 'E'],
   ['E', 'E', 'E'],
-  ['E', 'E', 'E'],
+  ['E', 'E', 'M'],
 ]
 
-const click: Coords = {board: board, row: 1, col: 1}
+console.table(board)
+
+const click: Coords = {board: board, row: 2, col: 2}
 
 function updateEmptySquare({board, row, col}: Coords) {
 
@@ -23,8 +25,8 @@ function updateEmptySquare({board, row, col}: Coords) {
     let adjacentMines = 0
     for (let x = -1; x < 2; x++) {
       for (let y = -1; y < 2; y++) {
-        if ((row + x) < 0 || (row + x) > board.length) continue
-        if (col + y < 0 || col + y > board[0].length) continue
+        if ((row + x) < 0 || (row + x) > board.length - 1) continue
+        if (col + y < 0 || col + y > board[0].length - 1) continue
         if (board[row + x][col + y] === 'M') adjacentMines += 1
       }
     }
@@ -35,10 +37,10 @@ function updateEmptySquare({board, row, col}: Coords) {
       for (let x = -1; x < 2; x++) {
         for (let y = -1; y < 2; y++) {
           if (x === 0 && y === 0) continue
-          if ((row + x) < 0 || (row + x) > board.length) continue
-          if ((col + y) < 0 || (col + y) > board[0].length) continue
-          
-
+          if ((row + x) < 0 || (row + x) > board.length - 1) continue
+          if ((col + y) < 0 || (col + y) > board[0].length - 1) continue
+          if (board[row + x][col + y] !== 'E') continue
+          updateEmptySquare({board: board, row: row + x, col: col + y})
         }
       }
     } else {
