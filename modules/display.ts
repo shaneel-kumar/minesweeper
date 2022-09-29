570a277ed247bf05f5c20b6a45ad5b5cc5a4ad1a
Shaneel Kumar <shaneelkumar.dev@gmail.com>
2022-09-29 21:56:51 +1300

    Add createGrid function to Display

--- modules/display.ts
+++ modules/display.ts
@@ -1,6 +1,20 @@
 function Display({width, height}: BoardDimensions) {
   const uiBoard = document.querySelector('.gameGrid') as Element
   
+  createGrid()
+  
+  function createGrid() {
+    for (let i = 0; i < height; i++) {
+      const row = document.createElement('div')
+      row.classList.add('gridRow')
+      for (let j = 0; j < width; j++) {
+        const cell = document.createElement('div')
+        cell.classList.add('cell')
+        row.appendChild(cell)
+      }
+      uiBoard.appendChild(row)
+    }
+  }
 }
 
 export default Display
